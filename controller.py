d0a54ec53c3aa24bb46b115b54cde150c7734d72
livialestingi <livia.lestingi@polimi.it>
2024-02-11 15:13:04 +0100

    wip simulator params processing

--- controller.py
+++ controller.py
@@ -1,27 +1,33 @@
 import os
+import pickle
 
-import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
 import analyser
 
 MODEL_PATH = 'submodules/gi_estimator/model/trained_model.h5'
-PROJECT_PATH = 'stratego_generator/' + MODEL_PATH
+ENCODER_PATH = 'submodules/gi_estimator/model/encoder.pickle'
+PROJECT_PATH = 'stratego_generator/'
 
 
 class AutonomicManagerController:
     def __init__(self):
         curr_path = os.getcwd()
         if 'impact' in curr_path:
-            model_path = os.getcwd().replace('impact2.10.7', PROJECT_PATH)
+            model_path = os.getcwd().replace('impact2.10.7', PROJECT_PATH + MODEL_PATH)
+            encoder_path = os.getcwd().replace('impact2.10.7', PROJECT_PATH + ENCODER_PATH)
         else:
             model_path = os.getcwd().replace('resources/robot_controllers', MODEL_PATH)
+            encoder_path = os.getcwd().replace('resources/robot_controllers', ENCODER_PATH)
 
         self.type_analyser: analyser.SyntheticTypeAnalyser = analyser.SyntheticTypeAnalyser(model_file=model_path)
 
-    def get_shared_identity_probability(self, sensor_data):
-        # type: (np.ndarray) -> float
+        # with open(encoder_path, "rb") as encoder_file:
+        #    self.encoder: OneHotEncoder = pickle.load(encoder_file, encoding='latin1')
 
-        type_probabilities = self.type_analyser.obtain_probabilities(sensor_data)  # type: np.ndarray
-        shared_identity_prob = type_probabilities.item()  # type: float
+    def get_shared_identity_probability(self, sensor_data):
+
+        type_probabilities = self.type_analyser.obtain_probabilities(sensor_data)
+        shared_identity_prob = type_probabilities.item()
 
         return shared_identity_prob
